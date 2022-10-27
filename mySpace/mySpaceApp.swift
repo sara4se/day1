f2770fe6601d85cf11931fe87e00f20b708fcd76
sara4se <s.m.m.h.h.18@gmail.com>
2022-10-27 09:54:27 +0300

    Initial Commit

---
+++ mySpace/mySpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySpaceApp.swift
+//  mySpace
+//
+//  Created by Sara Alhumidi on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct mySpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
